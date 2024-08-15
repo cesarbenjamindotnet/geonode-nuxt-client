3b617986b357d2d37f0de5f59ec0ec8b8334f02f
César Benjamín <mathereall@gmail.com>
2024-08-14 21:07:24 -0600

    777

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,5 +13,5 @@ export default defineNuxtConfig({
   },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt', "@nuxt/ui", "nuxt-quasar-ui"],
+  modules: ['@pinia/nuxt', "nuxt-quasar-ui"],
 })
