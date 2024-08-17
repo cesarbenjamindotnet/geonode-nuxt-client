68d65826ba3ee210d8e2a537b6488c9fc80828fb
Niji <mathereall@gmail.com>
2024-08-17 10:08:46 -0600

    inicial

--- plugins/quasar.ts
+++ plugins/quasar.ts
@@ -1,12 +1,15 @@
-import { Quasar } from 'quasar';
+// import {Quasar} from 'quasar';
 import '@quasar/extras/mdi-v5/mdi-v5.css'; // MDI v5
 import '@quasar/extras/material-icons/material-icons.css'; // Material Icons
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(Quasar, {
-    iconSet: 'material-icons', // Define el set predeterminado (Material Icons)
-    config: {
-      brand: {}, // Aquí puedes configurar los colores y otros ajustes globales
-    },
-  });
+    // TODO: revisar esto, parece que se está duplicando
+    /*
+    nuxtApp.vueApp.use(Quasar, {
+      config: {
+        iconSet: 'material-icons',
+        brand: {}, // Aquí puedes configurar los colores y otros ajustes globales
+      },
+    });
+    */
 });
