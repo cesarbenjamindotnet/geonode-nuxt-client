d1f73f0cf6df37dfc1f55f22705034fd6a874159
Niji <mathereall@gmail.com>
2024-08-29 21:00:55 -0600

    test callback.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,13 @@
 <template>
 <h1>Home</h1>
   <p>You are currently {{ status }}</p>
+  <p>Token: {{ authStore.token }}</p>
+  <p>User: {{ authStore.user }}</p>
 </template>
 
 <script setup lang="ts">
 const { status } = useAuth()
+const authStore = useAuthStore()
 </script>
 
 <style scoped>
