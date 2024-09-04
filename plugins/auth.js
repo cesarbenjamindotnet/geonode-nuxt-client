c69e675768f8b75a287c058d25ba2faad36f9e54
Niji <mathereall@gmail.com>
2024-09-04 10:41:23 -0600

    test callback.vue

--- plugins/auth.js
+++ plugins/auth.js
@@ -1,27 +1,26 @@
 export default defineNuxtPlugin(async (nuxtApp) => {
     const headers = useRequestHeaders(['cookie']);
-    const { status, data } = useAuth();
+    const {status, data} = useAuth();
     const authStore = useAuthStore();
 
     try {
-        if (status.value === 'authenticated') {
-            authStore.isAuthenticated = true;
-            console.log("authenticated");
-            // console.log("headers", headers);
-            // console.log("status", status);
-            // console.log("data", data.value.user);
+        if (status?.value === 'authenticated') {
+            console.log('is Authenticated:', status.value);
 
-            const { data: token } = await useFetch('/api/token', { headers });
+            authStore.isAuthenticated = true;
+
+            const {data: token} = await useFetch('/api/token', {headers});
 
             if (token) {
+
                 authStore.setToken(token);
                 authStore.setUser(data.value.user);
-                // console.log("token", token);
             } else {
-                throw new Error('Failed to fetch token');
+                throw new Error('Token is undefined');
             }
         }
     } catch (error) {
         console.error('Error during authentication process:', error);
     }
 });
+
